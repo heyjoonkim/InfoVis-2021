088836cb7afc718d805ee922316f38b9bd6a7e94
juny116 <juny116@gmail.com>
2021-12-06 22:59:34 +0900

    add max pooling

--- app.py
+++ app.py
@@ -16,17 +16,20 @@ def mean_pooling(model_output, attention_mask):
     sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
     return sum_embeddings / sum_mask
 
+def max_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    token_embeddings[input_mask_expanded == 0] = -1e9  # Set padding tokens to large negative value
+    return torch.max(token_embeddings, 1)[0]
 
 def get_embeddings(input_sent, prompt, method):
     concat_sent = f'{prompt} {input_sent}'
-    print(concat_sent)
     encoded_input = tokenizer(concat_sent, padding=True, truncation=True, max_length=128, return_tensors='pt').to('cuda')
 
     # Compute token embeddings
     with torch.no_grad():
         model_output = model(**encoded_input)
         sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
-    print(sentence_embeddings.shape)
 
     return sentence_embeddings
 
