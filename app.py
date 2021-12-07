855b2174fa5f0e99e2302b751c6c6967540a1783
juny116 <juny116@gmail.com>
2021-12-07 15:58:43 +0900

    add submit

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import torch
-from transformers import AutoTokenizer, AutoModel
-from flask import render_template, request, Flask
+from transformers import AutoTokenizer, AutoModel, AutoModelForMaskedLM
+from flask import render_template, request, Flask, Response, jsonify
+from datasets import dataset_dict, load_dataset, load_metric
+from utils import sst2_preprocess
+import umap
 
 global tokenizer 
 global model
+global dataset
+global current_model
+global umap_model
 
-tokenizer= AutoTokenizer.from_pretrained("bert-base-uncased")
-model = AutoModel.from_pretrained("bert-base-uncased")
-model = model.to('cuda')
+current_model = None
+model = None
+tokenizer = None
+dataset = None
+umap_model = None
 
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
@@ -22,16 +30,48 @@ def max_pooling(model_output, attention_mask):
     token_embeddings[input_mask_expanded == 0] = -1e9  # Set padding tokens to large negative value
     return torch.max(token_embeddings, 1)[0]
 
-def get_embeddings(input_sent, prompt, method):
-    concat_sent = f'{prompt} {input_sent}'
-    encoded_input = tokenizer(concat_sent, padding=True, truncation=True, max_length=128, return_tensors='pt').to('cuda')
+def process_dataset(prompt):
+    """
+        return list of embedding objects (sentence, label, umap, topk, attentions)
+    """
+    global dataset
+    global tokenizer 
+    global model
+    global umap_model
 
+    # preprocess data
+    dataset = dataset.map(sst2_preprocess, fn_kwargs={'tokenizer': tokenizer,'prompt':prompt})
+    # print(dataset['example_with_prompt'])
+    sentence_list = dataset['example_with_prompt']
+    label_list = dataset['label']
+    dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'mask_position', 'in_len', 'prompt_len'])
+    # dataloader
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=20)
+    data_list = []
     # Compute token embeddings
     with torch.no_grad():
-        model_output = model(**encoded_input)
-        sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+        for batch in dataloader:
+            # to cuda
+            batch = {k: v.cuda() for k, v in batch.items()}
+            # pop additional data
+            mask_position = batch.pop('mask_position')
+            in_len = batch.pop('in_len')
+            prompt_len = batch.pop('prompt_len')
+            # run model
+            model_output = model(**batch)
+            # get top 10 results
+            mask_position=torch.unsqueeze(mask_position, 1)
+            mask_position = mask_position.unsqueeze(2).expand(mask_position.size(0), mask_position.size(1), model_output.hidden_states[0].size(2))
+            hidden_states = torch.gather(model_output.hidden_states[0], 1, mask_position).squeeze()
 
-    return sentence_embeddings
+            data_list.append(hidden_states.cpu())
+
+    full_hidden_states = torch.cat(data_list)
+    umap_model = umap.UMAP().fit(full_hidden_states)
+    coords = umap_model.transform(full_hidden_states)
+
+    return sentence_list, label_list, coords
+
 
 
 app = Flask(__name__)
@@ -41,46 +81,62 @@ app = Flask(__name__)
 def home():
     """ This was the test page
     It can be accessed by http://127.0.0.1:5000/
-    May not work!!!!!
+    May not work!!!!! Use postman!!!
     """
     return render_template('home.html')
 
-
-@app.route('/single_sentence', methods=['POST'])
-def single_sentence():
+@app.route('/submit', methods=['POST'])
+def submit():
     """
     Process single sentence with prompt
     """
+    global dataset
+    global tokenizer 
+    global model
+    global current_model
 
-    input_sent = request.form['input_sent']
-    prompt = request.form['prompt']
-    method = request.form['method']
+    dataset_name = request.form['dataset']
+    model_name = request.form['model']
+    embedding = request.form['embedding']
+    prompt = request.form['inputPrompt']
 
-    emb = get_embeddings(input_sent, prompt, method)
 
-    return {'embeddings': emb.cpu().tolist()}
+    try:
+        dataset = load_dataset('glue', dataset_name, split='validation[:100]')
+    except:
+        return jsonify({'error':'dataset not found'}), 400
 
-@app.route('/set_model', methods=['POST'])
-def set_model():
-    """
-    Set global PLM
-    """
-    model_name = request.form['model_name']
-    success = False
-    if model_name == 'bert':
-        tokenizer= AutoTokenizer.from_pretrained("bert-base-uncased")
-        model = AutoModel.from_pretrained("bert-base-uncased")
-        success = True
-    elif model_name == 'roberta':
-        tokenizer= AutoTokenizer.from_pretrained("roberta-base")
-        model = AutoModel.from_pretrained("roberta-base")
-        success = True
-    else:
-        tokenizer= AutoTokenizer.from_pretrained("bert-base-uncased")
-        model = AutoModel.from_pretrained("bert-base-uncased")
-    model = model.to('cuda')
+    if model_name != current_model:
+        try:
+            model = AutoModelForMaskedLM.from_pretrained(model_name, output_hidden_states=True, output_attentions=True)
+            model.cuda()
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            current_model = model_name
+        except:
+            return jsonify({'error':'model not found'}), 400
 
-    return {'model_name': model_name, 'success': success}
+    sentence_list, label_list, coords = process_dataset(prompt)
+    final_list = []
+    for sentence, label, coord in zip(sentence_list, label_list, coords):
+        final_list.append({'sentence': sentence, 'label':label, 'x': float(coord[0]), 'y': float(coord[1])})
+
+    return jsonify({'embeddings':final_list}), 200
+
+
+# @app.route('/single_sentence', methods=['POST'])
+# def single_sentence():
+#     """
+#     Process single sentence with prompt
+#     """
+
+#     input_sent = request.form['input_sent']
+#     prompt = request.form['prompt']
+#     method = request.form['method']
+
+#     emb = get_embeddings(input_sent, prompt, method)
+
+#     return {'embeddings': emb.cpu().tolist()}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
